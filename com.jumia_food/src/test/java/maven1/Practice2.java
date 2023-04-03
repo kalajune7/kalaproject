6951b009b68409a27dc7cc090688d594a9829519
kiran kumar <gonipallikirann1997@gmail.com>
2023-04-03 21:31:44 +0530

    new class

---
+++ com.jumia_food/src/test/java/maven1/Practice2.java
@@ -0,0 +1,14 @@
+package maven1;
+
+import org.testng.annotations.Test;
+
+public class Practice2 {
+
+	@Test
+	public void kala()
+	{
+		System.out.println("--new project is added---");
+		
+		
+	}
+}
